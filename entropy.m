68fb1bfc53f21ee7bec5cbd21e0d129400a8eaae
rehsani <64758416+rehsani@users.noreply.github.com>
2021-02-22 18:47:58 -0700

    Add files via upload

---
+++ entropy.m
@@ -0,0 +1,27 @@
+% ****       Entropy Estimation Using Quantile Spacing Approach       ****
+% ****           © Hoshin Gupta & Mohammad Reza Ehsani 2021           ****
+% ****                                                                ****
+% ****        Version 1 written by Mohammad Reza Ehsani in 2020       ****
+% ****                                                                ****
+% ****                                                                ****
+% ****      \                                                 /       ****
+% ****       --- THIS IS THE MAIN FUNCTION TO EXECUTE QS ---        ****
+% ************************************************************************
+
+function [ sample , alpha , N_b , N_k ] = entropy(sample ,alpha ,N_b, N_k)
+    n = int64(ceil(alpha * size(sample)));
+    x_min = min(sample);
+    x_max = max(sample);
+    sort(sample)
+    for ii = 1:N_b
+        sample_b = datasample(sample(2:end-1), size(sample));
+        for jj = 1:N_k
+            X_alpha(jj, :) = datasample(sample_b, n, 'Replace', false);
+        end
+        X_alpha = sort(X_alpha, 2);
+        Z = [x_min mean(X_alpha, 1) x_max];
+        dZ = diff(Z);
+        H = (1/n) * sum(log(n * dZ));
+        h(ii) = H;
+    end
+    
